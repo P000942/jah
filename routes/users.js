4901ad9417b536e401306bdd6e8d7d2e1dbf471a
Geraldo Gomes <rangel@despertai.org.br>
2017-01-17 16:49:11 -0400

    criar CRUD

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
 module.exports = app => {
    const Users = app.db.models.Users;
-   app.get("/users"
+   app.get("/users/:id"
          , (req, res) => {
-           Users.findAll({}).then(users => {
-              res.json({users: users});
+           Users.findById(req.params.id
+                       , { attributes: ["id", "name", "email"] })
+                .then(result => res.json(result))
+                .catch(error => {
+                     res.status(412).json({msg: error.message});
+                 });
+          });
+   app.delete("/users/:id"
+           , (req, res) => {
+                 Users.destroy({where: {id: req.params.id} })
+                      .then(result => res.sendStatus(204))
+                      .catch(error => {
+                          res.status(412).json({msg: error.message});
+                      });
+             });
+   app.post("/users"
+          , (req, res) => {
+             Users.create(req.body)
+                  .then(result => res.json(result))
+                  .catch(error => {
+                      res.status(412).json({msg: error.message});
+                   });
             });
-        });
 };
