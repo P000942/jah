2d26d7491921f218959bf23581b4161c1194db8f
ran9el <rangel@despertai.org.br>
2020-07-22 15:23:30 -0400

    Reorganizacao do App, menus e html

--- public/js/jah/app/app.js
+++ public/js/jah/app/app.js
@@ -9,15 +9,16 @@ j$.$T => Atalho para os tabs que estao abertos. (j$.ui.Tabs.root.c$ - pq existe 
 j$.$V() => Atalho para mostrar no log j$.$C, j$.$P, j$.$R, j$.$S
 */
 
-var urlPartial = CONFIG.SERVER.CONTEXT+ 'partial.html';
-var adapter={
+const urlPartial   = CONFIG.SERVER.CONTEXT+ 'sample/partial.html';
+const urlPartial_1 = CONFIG.SERVER.CONTEXT+ 'sample/partial_1.html';
+const adapter={
     services:{
                  Papel:{caption:'Papel'    ,   crud:true, title:'Cadastro de Papel',  onClick:openPapel}
           ,    Usuario:{caption:'Usuário'  ,   crud:true, title:'Cadastro de Usuário'}
           ,  Documento:{caption:'Documento',   crud:true, title:'Cadastro de Documento'}
           ,    Assunto:{caption:'Assunto'  ,   crud:true, title:'Cadastro de Assunto', modal:true}
   ,  SituacaoAtividade:{caption:'Situação' ,   crud:true, title:'Cadastro de Situações da Atividade'}
-          ,    Partial:{partial:urlPartial ,              title:'Conheça a história e a caminhada da Comunidade'}
+          ,    Partial:{partial:urlPartial ,              title:'Partial - Qual a razao disso aqui'}
           ,     Pessoa:{caption:'Pessoa'   ,  query:true, title:'Exemplo de consulta'}
           ,     Basico:{caption:'Básico'   ,  query:true, title:'Consulta Básica', resource:'pessoa'}
           ,     Tarefa:{caption:'Tarefa'   ,   crud:true, title:'Tarefa'         , resource:'tarefa'}
@@ -42,24 +43,30 @@ $(document).ready(function(){
 ERROR.init({callback:function(msg,field){ERROR.on(msg, field);}});
 
 j$.ui.Page.Helper = function(){
-     var criarMenu = function(){
-             var menu_view = j$.Dashboard.Menubar.addMenu({caption:'Link'});
-             menu_view.add({caption:'Form basic' , url:CONFIG.SERVER.CONTEXT+ '/sample/formBasic.html', title:'Conheça a história e a caminhada da Comunidade'});
-             menu_view.add({caption:'Organização', url:CONFIG.SERVER.CONTEXT+ 'partial.html', title:'Veja como estamos organizados'});
-             menu_view.add({caption:'Álbum'      , url:CONFIG.SERVER.CONTEXT+ 'partial.html', title:'Veja um Álbum de nosso trabalho de evagelização!'});
+     const criarMenu = function(){
+             let _path = CONFIG.SERVER.CONTEXT
+               , menu_view = j$.Dashboard.Menubar.addMenu({caption:'Forms'});
+             menu_view.add({caption:'Form basic' , url:_path+ 'sample/formBasic.html' , title:'Form com campos formatados e controller'});
+             menu_view.add({caption:'Form Inject', url:_path+ 'sample/formInject.html', title:'Maninuplar informações do form'});
+             menu_view.add({caption:'Form Design', url:_path+ 'sample/formDesign.html', title:'Sobre como estilizar uma página'});
+             menu_view.add({caption:'Relatório'  , url:_path+ 'sample/reportTest.html', title:'Exemplo de relatório'});
+
+             menu_view = j$.Dashboard.Menubar.addMenu({caption:'Link Externo'});
+             menu_view.add({caption:'Link 1 '   , url:urlPartial  , title:'Vai abri uma página web com a URL'});
+             menu_view.add({caption:'Link 2'    , url:urlPartial_1, title:'Vai abri uma página web com a URL'});
 
              menu_view = j$.Dashboard.Menubar.addMenu({caption:'Partial'});
-             menu_view.add({caption:'Visão Geral',   partial:CONFIG.SERVER.CONTEXT+ 'partial.html', title:'Conheça a história e a caminhada da Comunidade'});
-             menu_view.add({caption:'Semenentinha',  partial:CONFIG.SERVER.CONTEXT+ 'partial.html', title:'Veja como estamos organizados'});
-             menu_view.add({caption:'Assunto',       title:'Veja um Álbum de nosso trabalho de evagelização!',
+             menu_view.add({caption:'Partial',   partial:urlPartial  , title:'Serah insedrido na tba'});
+             menu_view.add({caption:'Partial 2', partial:urlPartial_1, title:'Serah insedrido na tba'});
+             menu_view.add({caption:'Assunto',       title:'Assunto - exemplo colocar um  form na tab',
                   onClick:function(menu, event){
                       j$.Dashboard.Tabs.open({key:menu.key, caption:menu.caption
                           , onLoad: function(tab){Assunto.init(tab.idContent);}});
                   }
               });
-             menu_view.add({caption:'Partial', byPass:true,      partial:CONFIG.SERVER.CONTEXT+ 'partial.html'
-                , onClick:j$.Dashboard.Tabs.openPartial
-             });
+            //  menu_view.add({caption:'Partial', byPass:true,      partial:urlPartial
+            //     , onClick:j$.Dashboard.Tabs.openPartial
+            //  });
 
              menu_view.add({key:'Tabela', caption:'Tabela', icon:'icon-pencil', title:'Tabela de Exemplo'
                 , onClick:j$.Dashboard.Tabs.delegateTo
@@ -71,11 +78,11 @@ j$.ui.Page.Helper = function(){
              j$.Dashboard.Menubar.render();
     };
 
-    var criarTab= function (){
+    const criarTab= function (){
          j$.Dashboard.Tabs.open({key:"tab_inicial", caption:"Home", fixed:true
-           , onLoad      :function(tab){tab.showURL("partial_1.html");}
-           , onActivate  :function(tab){console.log("onActivate."+tab.key);}
-           , onDeactivate:function(tab){console.log("onDeactivate."+tab.key);}
+           , onLoad      (tab){tab.showURL("sample/partial_1.html", (r)=>{console.log("callback do load complete", r)})}
+           , onActivate  (tab){console.log("onActivate."+tab.key)}
+           , onDeactivate(tab){console.log("onDeactivate."+tab.key)}
              });
     // DID�TICO: Apenas para saber como para colocar num tab
     //     tabs.add({key:"tab_Assunto", caption:"Assunto",
